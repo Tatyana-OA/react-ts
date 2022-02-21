c41fa0e736b34ee43d8c1151a57166d5557afc9a
Tatyana Asenova <tatyana.asenova@mentormate.com>
2022-02-21 14:44:16 +0200

    Added appropriate message for when there are no todos in list

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,29 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import './ToDoList.css'
+import "./ToDoList.css";
 
 // Interface describing the type of props received
 interface TodoListProps {
-    items: {id: string, text: string}[];
-    onDeleteTodo: (id:string) => void
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
-const ToDoList: React.FC<TodoListProps> = ({items, onDeleteTodo}) => { // referring to the interface
+const ToDoList: React.FC<TodoListProps> = ({ items, onDeleteTodo }) => {
+  // referring to the interface
 
   return (
-      <>
-      <h1 className="list-title">Your Todos:</h1>
-    <ul>
-        {items.map(t => (
-            <li key={t.id}>
+    <>
+      {items.length > 0 ? (
+        <>
+          <h1 className="list-title">Your Todos:</h1>
+          <ul>
+            {items.map((t) => (
+              <li key={t.id}>
                 <span>{t.text}</span>
                 {/* Using bind ensures that the way the function is defined (receives id) doesn't irritate TS :D  */}
                 <button onClick={onDeleteTodo.bind(null, t.id)}>DELETE</button>
-            </li>
-        ))}
-    </ul>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h1 className="list-title">No Todos found.</h1>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
